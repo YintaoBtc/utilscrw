1d9e6d643f60d64aa50e1f6be2c9a3636cf9b055
yintaobtc <yintao@protonmail.com>
2019-06-29 19:28:37 +0200

    Added profiles and registration

---
+++ registration/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+
+def custom_upload_to(instance, filename):
+    old_instance = Profile.objects.get(pk=instance.pk)
+    old_instance.avatar.delete()
+    return 'profiles/' + filename
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to=custom_upload_to, null=True, blank=True)
+    bio = models.TextField(null=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
+
+    addr_withdraw = models.CharField(max_length=100, null=True, blank=True)
+    addr_base = models.CharField(max_length=100, null=True, blank=True)
+    balance = models.FloatField(default=0)
+
+    public = models.BooleanField(default=False)
+
+
+@receiver(post_save, sender=User)
+def ensure_profile_exists(sender, instance, **kwargs):
+    if kwargs.get("created", False):
+        Profile.objects.get_or_create(user=instance)
+        print("Se acaba de crear un perfil")
+
+
--- utilscrw/settings.py
+++ utilscrw/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'core',
     'blocks.apps.BlocksConfig',
     'projects.apps.ProjectsConfig',
+    'profiles'
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +124,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Auth Redirects
+#LOGIN_REDIRECT_URL = 'projects:projects'
+LOGOUT_REDIRECT_URL = 'home'
+
+# Backend Email in files.
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    #CONFIGURATE PRODUCTION EMAIL
+    pass
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
